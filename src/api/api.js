2218f5d44ccdce1131e7e1555fd53421ffaa125a
daridev <darideveloper@gmail.com>
2026-07-16 06:53:44 -0600

    chore(api): enable production API URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
-// export const apiBaseUrl = 'https://omar.apps.darideveloper.com/loris'
-export const apiBaseUrl = 'http://localhost:8000/loris'
+export const apiBaseUrl = 'https://omar.apps.darideveloper.com/loris'
+// export const apiBaseUrl = 'http://localhost:8000/loris'
 export const saleEndpoint = `${apiBaseUrl}/sale/`
